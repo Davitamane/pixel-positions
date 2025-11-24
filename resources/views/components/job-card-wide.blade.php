1871825b3244fae441aa14a736e564dcb7c9f1a9
davita <datokhizanishvili005@gmail.com>
2025-11-24 16:12:57 +0400

    working on static ui

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,18 @@
+<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+    <img src="https://via.placeholder.com/600/92c952" alt="" class="rounded-xl  ">
+    <div>
+        <a href="#" class="self-start text-sm">Laracasts</a>
+
+        <h3>Video Producer</h3>
+        <p>Full time - From $60,000</p>
+    </div>
+
+    <div class="flex justify-between items-center mt-auto">
+        <div>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+        </div>
+
+    </div>
+</div>
---
+++ resources/views/components/tag.blade.php
@@ -0,0 +1,1 @@
+<a href="" class="bg-white/10 px-2 py-1 rounded-xl text-xs hover:bg-white/25 transition-all duration-300">{{$slot}}</a>
